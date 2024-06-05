c3e43d54eb651497dd99c0cc28c16b11181b6d66
Avish Madaan <theavishmadaan@gmail.com>
2024-06-05 11:37:50 +0530

    Updated Gateaway and Eureka

---
+++ ApiGateaway/src/main/java/com/example/ApiGateaway/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.example.ApiGateaway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+
+        return builder.routes()
+                .route(p -> p.path("/api/v1/**")
+                .uri("lb://user-authentication-service/"))
+
+                .build();
+    }
+}
